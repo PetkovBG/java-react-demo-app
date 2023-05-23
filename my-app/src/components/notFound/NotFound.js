49e9ccf8aaa7a4a12b47aa933293a747ce1c02c2
opetkov3 <orlin.petkov2@dxc.com>
2023-05-23 09:48:43 +0300

    Add link to home page to NotFound component

--- my-app/src/components/notFound/NotFound.js
+++ my-app/src/components/notFound/NotFound.js
@@ -1,11 +1,15 @@
-
+import { Link } from "react-router-dom";
+import styles from './NotFound.module.css';
 
 const NotFound = () => {
 
     return(
-        <div>
+        <>
+        <div >
             This page could not be found.
         </div>
+        <Link to="/" className={styles.homeLink}>Home</Link>
+        </>
     )
 
 }
